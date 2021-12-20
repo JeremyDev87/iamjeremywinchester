b76399d4bb5bf8b694aea42adff6f1489cd18bd6
Jeremy Park <jeremypark@192.168.nate.com>
2021-12-20 16:17:57 +0900

    soft background border

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,13 @@
 window.addEventListener("scroll", function () {
     var richking = document.querySelector(".secondContents");
+    var javascript = document.querySelector(".thirdContents");
     var snow = document.querySelector("#outer");
     var scrollLocation = document.documentElement.scrollTop;
     if (scrollLocation > 50 && scrollLocation < 900 && richking.className == "secondContents") {
         richking.className += " fadeInStyle";
         snow.className = "";
+    }
+    else if (scrollLocation > 800 && scrollLocation < 1500 && javascript.className == "thirdContents") {
+        javascript.className += " fadeInStyle";
     }
 });
--- public/ts/index.ts
+++ public/ts/index.ts
@@ -1,10 +1,13 @@
 
 window.addEventListener("scroll",() => {
     let richking:any = document.querySelector(".secondContents");
+    let javascript:any = document.querySelector(".thirdContents");
     let snow:any = document.querySelector("#outer");
     let scrollLocation:number = document.documentElement.scrollTop;
     if(scrollLocation>50  && scrollLocation <900 && richking.className=="secondContents") {
         richking.className += " fadeInStyle";
         snow.className="";
+    }else if(scrollLocation>800  && scrollLocation <1500 && javascript.className=="thirdContents"){
+        javascript.className += " fadeInStyle";
     }
 })
