0579322e554b6902b66d0eec674010790d9a5177
Jeremy Park <jeremypark@192.168.nate.com>
2021-12-20 16:47:11 +0900

    ready tech stack animation

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,11 @@ window.addEventListener("scroll", function () {
     var javascript = document.querySelector(".thirdContents");
     var snow = document.querySelector("#outer");
     var scrollLocation = document.documentElement.scrollTop;
-    if (scrollLocation > 50 && scrollLocation < 900 && richking.className == "secondContents") {
+    if (scrollLocation >= 1 && scrollLocation < 700 && richking.className == "secondContents") {
         richking.className += " fadeInStyle";
         snow.className = "";
     }
-    else if (scrollLocation > 800 && scrollLocation < 1500 && javascript.className == "thirdContents") {
+    else if (scrollLocation >= 700 && scrollLocation < 1500 && javascript.className == "thirdContents") {
         javascript.className += " fadeInStyle";
     }
 });
