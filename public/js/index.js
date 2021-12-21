1776462be62137d8d0332466da8f55f5eb4c9adf
Jeremy Park <jeremypark@192.168.nate.com>
2021-12-21 11:15:39 +0900

    third page complete

--- public/js/index.js
+++ public/js/index.js
@@ -1,13 +1,18 @@
-window.addEventListener("scroll", function () {
+var GetScrollLocation = function () {
+    return document.documentElement.scrollTop;
+};
+var ShowBgContents = function () {
+    var scrollLocation = GetScrollLocation();
     var richking = document.querySelector(".secondContents");
     var javascript = document.querySelector(".thirdContents");
     var snow = document.querySelector("#outer");
-    var scrollLocation = document.documentElement.scrollTop;
-    if (scrollLocation >= 1 && scrollLocation < 700 && richking.className == "secondContents") {
+    if (scrollLocation >= 1 && richking.className == "secondContents") {
         richking.className += " fadeInStyle";
         snow.className = "";
     }
-    else if (scrollLocation >= 700 && scrollLocation < 1500 && javascript.className == "thirdContents") {
+    if (scrollLocation >= 700 && javascript.className == "thirdContents") {
         javascript.className += " fadeInStyle";
     }
-});
+};
+window.addEventListener("scroll", ShowBgContents);
+ShowBgContents();
--- public/ts/index.ts
+++ public/ts/index.ts
@@ -1,13 +1,19 @@
+const GetScrollLocation = ():number => {
+    return document.documentElement.scrollTop;
+}
 
-window.addEventListener("scroll",() => {
+const ShowBgContents = ():void => {
+    let scrollLocation:number = GetScrollLocation();
     let richking:any = document.querySelector(".secondContents");
     let javascript:any = document.querySelector(".thirdContents");
     let snow:any = document.querySelector("#outer");
-    let scrollLocation:number = document.documentElement.scrollTop;
-    if(scrollLocation>=1  && scrollLocation <700 && richking.className=="secondContents") {
+    if(scrollLocation>=1  && richking.className=="secondContents") {
         richking.className += " fadeInStyle";
         snow.className="";
-    }else if(scrollLocation>=700  && scrollLocation <1500 && javascript.className=="thirdContents"){
+    }
+    if(scrollLocation>=700  && javascript.className=="thirdContents"){
         javascript.className += " fadeInStyle";
     }
-})
+}
+window.addEventListener("scroll",ShowBgContents);
+ShowBgContents();
