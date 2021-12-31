f1df723164265ea629d211ea45197227660f778e
Jeremy Winchester Park <soundbrokaz@kakao.com>
2021-12-31 14:03:00 +0900

    port 80

--- public/js/index.js
+++ public/js/index.js
@@ -30,6 +30,54 @@ var btnEvent = function (parm) {
     nowImgNum = nowImgNum < 1 ? 10 : nowImgNum;
     nowImgNum = nowImgNum > 10 ? 1 : nowImgNum;
     imgDiv.style.backgroundImage = "url('/img/portfolio/".concat(nowImgNum, ".png')");
+    var subtitle = document.querySelector(".fourthSubTitle span");
+    var subDescription = document.querySelector(".fourthSubTitle p");
+    switch (nowImgNum) {
+        case 1:
+            subtitle.innerHTML = "test1";
+            subDescription.innerHTML = "test1";
+            break;
+        case 2:
+            subtitle.innerHTML = "test2";
+            subDescription.innerHTML = "test2";
+            break;
+        case 3:
+            subtitle.innerHTML = "test3";
+            subDescription.innerHTML = "test3";
+            break;
+        case 4:
+            subtitle.innerHTML = "test4";
+            subDescription.innerHTML = "test4";
+            break;
+        case 5:
+            subtitle.innerHTML = "test5";
+            subDescription.innerHTML = "test5";
+            break;
+        case 6:
+            subtitle.innerHTML = "test6";
+            subDescription.innerHTML = "test6";
+            break;
+        case 7:
+            subtitle.innerHTML = "test7";
+            subDescription.innerHTML = "test7";
+            break;
+        case 8:
+            subtitle.innerHTML = "test8";
+            subDescription.innerHTML = "test8";
+            break;
+        case 9:
+            subtitle.innerHTML = "test9";
+            subDescription.innerHTML = "test9";
+            break;
+        case 10:
+            subtitle.innerHTML = "test10";
+            subDescription.innerHTML = "test10";
+            break;
+        default:
+            subtitle.innerHTML = "asdf";
+            subDescription.innerHTML = "asdf";
+            break;
+    }
 };
 leftBtn.addEventListener("click", function () { btnEvent('before'); });
 rightBtn.addEventListener("click", function () { btnEvent('next'); });
